0db3e6166b5a70aa545d5096d66028772f024280
SantiagoCaso <santiagoignaciocaso@gmail.com>
2023-06-08 11:46:30 -0300

    Cambios de prueba 2

--- src/components/productCard/ProductCard.js
+++ src/components/productCard/ProductCard.js
@@ -1,25 +1,26 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import ListGroup  from 'react-bootstrap/ListGroup';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
 
-const ProductCard = ({id, image, name, size, color}) => {
+const ProductCard = ({ id, image, name, size, color }) => {
   return (
-    <Card style={{width: '18rem'}}>
-        <Card.Img variant='top' src={image}/>
-        <Card.Body>
-            <Card.Title>{name}</Card.Title>
-        </Card.Body>
-        <ListGroup variant="flush">
-            <ListGroup.Item>talle</ListGroup.Item>
-            <ListGroup.Item>color</ListGroup.Item>
-        
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={image} />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+      </Card.Body>
+      <ListGroup variant="flush">
+        <ListGroup.Item>talle</ListGroup.Item>
+        <ListGroup.Item>color</ListGroup.Item>
+        <ListGroup.Item>cantidad</ListGroup.Item>
+        <ListGroup.Item>afgaf</ListGroup.Item>
+        <ListGroup.Item>afg</ListGroup.Item>
       </ListGroup>
       <Card.Body>
         <Button /*{onClick={}}*/>Comprar</Button>
       </Card.Body>
-       
     </Card>
-  )
-}
+  );
+};
 
-export default ProductCard
+export default ProductCard;
