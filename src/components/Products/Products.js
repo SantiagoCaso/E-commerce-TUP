2b815e2f4478cc38ad800d843080e785f8e0debe
SantiagoCaso <santiagoignaciocaso@gmail.com>
2023-06-08 11:41:07 -0300

    cambios de prueba

---
+++ src/components/Products/Products.js
@@ -0,0 +1,2 @@
+import "./Products.css"
+import ProductItem from "..productItems/ProductItem.js"
--- src/components/productItems/ProductItem.js
+++ src/components/productItems/ProductItem.js
@@ -2,24 +2,34 @@ import { useEffect, useState } from "react";
 import ProductCard from "../productCard/ProductCard";
 
 const ProductItem = () => {
-  const [product, setProduct]= useState([]);
-  const getProduct = async () =>{
-    const response = await fetch('https://rickandmortyapi.com/api/character')
-    const data = await response.json()
+  const [product, setProduct] = useState([]);
+  const getProduct = async () => {
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await response.json();
 
-    setProduct(data.result)
-  }
-useEffect(()=>{
-   getProduct()
-}, [])
+    setProduct(data.result);
+  };
+  useEffect(() => {
+    getProduct();
+  }, []);
 
   return (
     <>
-      {product.map(p=> <ProductCard key={p.id} image={p.image} name={p.name} color={p.color} size={p.size}/>)}
+      {product.map((p) => (
+        <ProductCard
+          key={p.id}
+          image={p.image}
+          name={p.name}
+          color={p.color}
+          size={p.size}
+        />
+      ))}
     </>
-      
-    
   );
 };
+
+// const ProductItem = () => {
+
+// }
 
 export default ProductItem;
